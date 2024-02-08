f10c178cfdafcbb91aa85c7ed1773e92d9bf3f3e
kendpr <real.kendpr@gmail.com>
2024-02-09 01:07:58 +0800

    Navigation menu popup
    - Used dialog instead of a div
    - Set the dialog to display block
    - and modified its opacity and visibility for fade-transition to work

--- script.js
+++ script.js
@@ -1,24 +1,23 @@
 const menuBtn = document.getElementsByClassName("menu-button");
 const openBtn = document.getElementsByClassName("open-button")[0];
 const menu = document.getElementsByClassName("menu")[0];
-const voidBg = document.getElementsByClassName("void")[0];
 
-function closeMenu() {
-  menu.classList.remove("active");
-  voidBg.classList.remove("active");
-  openBtn.classList.remove("noBtn");
-}
 function openMenu() {
-  openBtn.classList.add("noBtn");
+  menu.showModal();
   menu.classList.add("active");
-  voidBg.classList.add("active");
+  openBtn.classList.add("noBtn");
+}
+function closeMenu() {
+  menu.close();
+  menu.classList.remove("active");
+  openBtn.classList.remove("noBtn");
 }
 
 for (let i = 0; i < menuBtn.length; i++) {
   const twoButton = menuBtn[i];
 
   twoButton.addEventListener("click", () => {
-    if (menu.classList.contains("active")) {
+    if (menu.hasAttribute("open")) {
       closeMenu();
     } else {
       openMenu();
@@ -26,6 +25,14 @@ for (let i = 0; i < menuBtn.length; i++) {
   });
 }
 
-voidBg.addEventListener("click", () => {
-  closeMenu();
+menu.addEventListener("click", (event) => {
+  var rect = menu.getBoundingClientRect();
+  var isInMenu =
+    rect.top <= event.clientY &&
+    event.clientY <= rect.top + rect.height &&
+    rect.left <= event.clientX &&
+    event.clientX <= rect.left + rect.width;
+  if (!isInMenu) {
+    closeMenu();
+  }
 });
