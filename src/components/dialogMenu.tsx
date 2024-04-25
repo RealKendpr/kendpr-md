259c05ae8bc83849919cd942fe554a4ca77bc7d4
kendpr <real.kendpr@gmail.com>
2024-04-25 22:23:41 +0800

    Made the modal menu worked - Styling

--- src/components/dialogMenu.tsx
+++ src/components/dialogMenu.tsx
@@ -1,41 +1,73 @@
-export function DialogMenu() {
+import { useEffect, useRef } from "react";
+
+export function DialogMenu({
+  dialogStatus,
+  setDialogStatus,
+}: {
+  dialogStatus: string;
+  setDialogStatus: React.Dispatch<React.SetStateAction<string>>;
+}) {
+  const dialog = useRef<HTMLDialogElement>(null);
+
+  useEffect(() => {
+    dialogStatus === "open"
+      ? dialog.current?.showModal()
+      : dialog.current?.close();
+    const handleKeyDown = (e: KeyboardEvent) => {
+      if (e.key === "Escape") e.preventDefault();
+    };
+    dialogStatus === "open"
+      ? addEventListener("keydown", handleKeyDown)
+      : removeEventListener("keydown", handleKeyDown);
+  }, [dialogStatus]);
+
   return (
-    <dialog class="z-50 fixed inset-0 flex-col-reverse py-4 px-[clamp(1rem,_5%,_2rem)] w-3/4 max-w-[500px] rounded-2xl border">
-      <div>
-        <ul class="grid 5px">
-          <li>
-            <a href="#about">About Me</a>
-          </li>
-          <li>
-            <a href="#works">Works</a>
-          </li>
-        </ul>
-        <ul class="mt-1 flex gap-x-4">
-          <li class="w-8">
-            <a href="https://github.com/RealKendpr">
-              <picture>
-                <source
-                  srcset="./assets/github-icon-light.svg"
-                  media="(prefers-color-scheme: dark)"
-                />
-                <img src="./assets/github-icon-dark.svg" alt="" />
-              </picture>
-            </a>
-          </li>
-          <li class="w-8">
-            <a href="https://www.linkedin.com/in/kendpr/">
-              <img src="./assets/linkedin-icon.svg" alt="" />
-            </a>
-          </li>
-          <li class="w-8">
-            <a href="https://www.fiverr.com/kendpr">
-              <img src="./assets/fiverr-icon.svg" alt="" />
-            </a>
-          </li>
-        </ul>
-      </div>
-      <div class="text-right">
-        <button class="menu-button">&#10060;</button>
+    <dialog
+      ref={dialog}
+      className="fixed z-50 inset-0 rounded-2xl py-4 px-[clamp(1rem,_5%,_2rem)] border w-3/4 max-w-[500px]"
+    >
+      <div className="flex flex-col-reverse">
+        <div>
+          <ul className="grid 5px">
+            <li>
+              <a href="#about">About Me</a>
+            </li>
+            <li>
+              <a href="#works">Works</a>
+            </li>
+          </ul>
+          <ul className="mt-1 flex gap-x-4">
+            <li className="w-8">
+              <a href="https://github.com/RealKendpr">
+                <picture>
+                  <source
+                    srcSet="./assets/github-icon-light.svg"
+                    media="(prefers-color-scheme: dark)"
+                  />
+                  <img src="./assets/github-icon-dark.svg" alt="" />
+                </picture>
+              </a>
+            </li>
+            <li className="w-8">
+              <a href="https://www.linkedin.com/in/kendpr/">
+                <img src="./assets/linkedin-icon.svg" alt="" />
+              </a>
+            </li>
+            <li className="w-8">
+              <a href="https://www.fiverr.com/kendpr">
+                <img src="./assets/fiverr-icon.svg" alt="" />
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="text-right">
+          <button
+            onClick={() => setDialogStatus("close")}
+            className="menu-button"
+          >
+            &#10060;
+          </button>
+        </div>
       </div>
     </dialog>
   );
---
+++ src/components/navigation.tsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import { DialogMenu } from "./dialogMenu";
+
+export function Navigation() {
+  const [dialogStatus, setDialogStatus] = useState<string>("close");
+
+  return (
+    <nav className="fixed left-0 top-0 right-0 bg-black z-40">
+      <div className="py-2 px-[2%] max-w-lg flex justify-between">
+        <a className="block w-8" href="#">
+          <picture>
+            <source
+              srcSet="./assets/logo-is_light.svg"
+              media="(prefers-color-scheme: dark)"
+            />
+            <img src="./assets/logo-is_dark.svg" alt="Kendpr's Logo" />
+          </picture>
+        </a>
+        {dialogStatus === "close" && (
+          <button
+            onClick={() => setDialogStatus("open")}
+            className="open-button text-2xl hover:cursor-pointer"
+          >
+            &#129386;
+          </button>
+        )}
+      </div>
+      <DialogMenu
+        dialogStatus={dialogStatus}
+        setDialogStatus={setDialogStatus}
+      />
+    </nav>
+  );
+}
