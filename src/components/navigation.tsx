d0c68914a813d717ba1c34e683b18093520ece9b
kendpr <real.kendpr@gmail.com>
2024-04-27 23:25:00 +0800

    Prevent srcolling of body when dialog menu is open

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -1,12 +1,18 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { DialogMenu } from "./dialogMenu";
 import { Logo } from "./logo";
 
 export function Navigation() {
   const [dialogStatus, setDialogStatus] = useState<string>("close");
 
+  useEffect(() => {
+    if (dialogStatus === "open") {
+      document.body.style.overflow = "hidden";
+    } else document.body.style.overflow = "auto";
+  }, [dialogStatus]);
+
   return (
-    <nav className="fixed left-0 right-0 top-0 z-40 border-b border-b-neutral-200 bg-neutral-500 bg-opacity-70 backdrop-blur-md dark:border-b-neutral-700 dark:bg-neutral-800">
+    <nav className="fixed left-0 right-0 top-0 z-40 border-b border-b-neutral-200 bg-neutral-500 bg-opacity-70 backdrop-blur-md dark:border-b-neutral-700 dark:bg-neutral-800 dark:bg-opacity-50">
       <div className="mx-auto flex max-w-screen-sm justify-between px-[2%] py-2 sm:px-0">
         <a href="#b">
           <Logo location="navigation" />
