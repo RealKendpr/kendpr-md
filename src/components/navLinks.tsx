c966a10ce24b8a2cccf96c8ee25d652e312cbc37
kendpr <real.kendpr@gmail.com>
2024-04-27 14:06:46 +0800

    Footer Styling Added a border in the footer and made the footer 100% of the screen

--- src/components/navLinks.tsx
+++ src/components/navLinks.tsx
@@ -1,6 +1,6 @@
 export function NavLinks() {
   return (
-    <ul className="grid gap-1">
+    <ul className="grid gap-1 place-self-end">
       <li>
         <a href="#about">About Me</a>
       </li>
