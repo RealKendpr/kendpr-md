1f36641559f92186a012647718c7d55ef56c0e99
Ken <real.kendpr@gmail.com>
2025-05-19 17:49:12 +0800

    fix(post list): post urls not pointed to correct path

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,7 +2,7 @@ import { defineCollection, z } from "astro:content";
 import { glob } from "astro/loaders";
 
 const posts = defineCollection({
-  loader: glob({ pattern: "./pages/posts/*.mdx", base: "./src/" }),
+  loader: glob({ pattern: "./posts/*.mdx", base: "./src/pages/" }),
   schema: z.object({
     title: z.string(),
     description: z.string(),
